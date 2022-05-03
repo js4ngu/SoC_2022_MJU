c2e1150186e39bc66dd384d66bcd1249fa3d36ad
Jongsang Yoo <jamin0513@gmail.com>
2022-05-03 10:27:44 +0900

    LAB4

--- Lab3/Lab3.sdk/Hello_world/src/helloworld.c
+++ Lab3/Lab3.sdk/Hello_world/src/helloworld.c
@@ -5,15 +5,13 @@
 
 //void print(char *str);
 
-int main()
-{
-
-static XGpioPs psGpioInstancePtr;
-XGpioPs_Config *GpioConfigPtr;
-int xStatus;
-int BtnPinDirection = 0, BtnPinNumber = 50;
-int LedPinDirection = 1, LedPinNumber = 7;
-int ReadStatus=0, OldStatus=0;
+int main() {
+    static XGpioPs psGpioInstancePtr;
+    XGpioPs_Config *GpioConfigPtr;
+    int xStatus;
+    int BtnPinDirection = 0, BtnPinNumber = 50;
+    int LedPinDirection = 1, LedPinNumber = 7;
+    int ReadStatus=0, OldStatus=0;
 
     init_platform();
 
@@ -22,47 +20,42 @@ int ReadStatus=0, OldStatus=0;
 
     // PS GPIO Initialization
 
-    GpioConfigPtr = XGpioPs_LookupConfig(XPAR_PS7_GPIO_0_DEVICE_ID);
+    GpioConfigPtr = XGpioPs_LookupConfig(XPAR_PS7_GPIO_0_DEVICE_ID); //GPIO confiquration pointer를 찾음
     if (GpioConfigPtr==NULL) return XST_FAILURE;
 
     xStatus = XGpioPs_CfgInitialize(&psGpioInstancePtr, GpioConfigPtr, GpioConfigPtr->BaseAddr);
-    if (XST_SUCCESS != xStatus)
-    print("PS GPIO INIT FAILED \n\r");
+    if (XST_SUCCESS != xStatus) print("PS GPIO INIT FAILED \n\r");
 
 
     // PS GPIO pin setting
-       // Push button input
-       XGpioPs_SetDirectionPin(&psGpioInstancePtr, BtnPinNumber, BtnPinDirection);
-       XGpioPs_SetOutputEnablePin(&psGpioInstancePtr, BtnPinNumber, 0);
+    // Push button input
+    XGpioPs_SetDirectionPin(&psGpioInstancePtr, BtnPinNumber, BtnPinDirection);
+    XGpioPs_SetOutputEnablePin(&psGpioInstancePtr, BtnPinNumber, 0);
 
-       // LED output
-       XGpioPs_SetDirectionPin(&psGpioInstancePtr, LedPinNumber, LedPinDirection);
-       XGpioPs_SetOutputEnablePin(&psGpioInstancePtr, LedPinNumber, 0);
+    // LED output
+    XGpioPs_SetDirectionPin(&psGpioInstancePtr, LedPinNumber, LedPinDirection);
+    XGpioPs_SetOutputEnablePin(&psGpioInstancePtr, LedPinNumber, 0);
 
-       xStatus = 0;
+    xStatus = 0;
 
-       while(1)
-       {
-       ReadStatus = XGpioPs_ReadPin(&psGpioInstancePtr, BtnPinNumber);
-       if ((ReadStatus==1) && (OldStatus == 0))
-       {
-       print("BTN8_PUSH Button pressed \r\n");
-       if (xStatus==1)
-       {
-       xStatus = 0;
-       XGpioPs_WritePin(&psGpioInstancePtr, LedPinNumber, 0);
-       print("LED is OFF \r\n");
-       }
-       else
-       {
-       xStatus = 1;
-       XGpioPs_WritePin(&psGpioInstancePtr, LedPinNumber, 1);
-       print("LED is ON \r\n");
-       }
-       }
-       OldStatus = ReadStatus;
-       }
+    while(1) {
+        ReadStatus = XGpioPs_ReadPin(&psGpioInstancePtr, BtnPinNumber);
+        if ((ReadStatus==1) && (OldStatus == 0)) {
+            print("BTN8_PUSH Button pressed \r\n");
+            if (xStatus==1){
+                xStatus = 0;
+                XGpioPs_WritePin(&psGpioInstancePtr, LedPinNumber, 0);
+                print("LED is OFF \r\n");
+            }
+            else {
+                xStatus = 1;
+                XGpioPs_WritePin(&psGpioInstancePtr, LedPinNumber, 1);
+                print("LED is ON \r\n");
+            }
+        }
+        OldStatus = ReadStatus;
+    }
 
-       cleanup_platform();
-       return 0;
-   }
+   cleanup_platform();
+   return 0;
+}
---
+++ lab4/lab4.srcs/sources_1/bd/design_1/hdl/design_1_wrapper.v
@@ -0,0 +1,44 @@
+//Copyright 1986-2018 Xilinx, Inc. All Rights Reserved.
+//--------------------------------------------------------------------------------
+//Tool Version: Vivado v.2018.2 (win64) Build 2258646 Thu Jun 14 20:03:12 MDT 2018
+//Date        : Tue May  3 09:56:15 2022
+//Host        : DESKTOP-437TBDQ running 64-bit major release  (build 9200)
+//Command     : generate_target design_1_wrapper.bd
+//Design      : design_1_wrapper
+//Purpose     : IP block netlist
+//--------------------------------------------------------------------------------
+`timescale 1 ps / 1 ps
+
+module design_1_wrapper
+   (FIXED_IO_0_ddr_vrn,
+    FIXED_IO_0_ddr_vrp,
+    FIXED_IO_0_mio,
+    FIXED_IO_0_ps_clk,
+    FIXED_IO_0_ps_porb,
+    FIXED_IO_0_ps_srstb,
+    LED);
+  inout FIXED_IO_0_ddr_vrn;
+  inout FIXED_IO_0_ddr_vrp;
+  inout [53:0]FIXED_IO_0_mio;
+  inout FIXED_IO_0_ps_clk;
+  inout FIXED_IO_0_ps_porb;
+  inout FIXED_IO_0_ps_srstb;
+  output [7:0]LED;
+
+  wire FIXED_IO_0_ddr_vrn;
+  wire FIXED_IO_0_ddr_vrp;
+  wire [53:0]FIXED_IO_0_mio;
+  wire FIXED_IO_0_ps_clk;
+  wire FIXED_IO_0_ps_porb;
+  wire FIXED_IO_0_ps_srstb;
+  wire [7:0]LED;
+
+  design_1 design_1_i
+       (.FIXED_IO_0_ddr_vrn(FIXED_IO_0_ddr_vrn),
+        .FIXED_IO_0_ddr_vrp(FIXED_IO_0_ddr_vrp),
+        .FIXED_IO_0_mio(FIXED_IO_0_mio),
+        .FIXED_IO_0_ps_clk(FIXED_IO_0_ps_clk),
+        .FIXED_IO_0_ps_porb(FIXED_IO_0_ps_porb),
+        .FIXED_IO_0_ps_srstb(FIXED_IO_0_ps_srstb),
+        .LED(LED));
+endmodule
