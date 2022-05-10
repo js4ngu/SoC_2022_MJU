ed93c8ffe2021a73f5ec98517cb91c648c45f5d1
Jongsang Yoo <jamin0513@gmail.com>
2022-05-10 15:56:09 +0900

    ALU input 구현

--- HW1/memo.c
+++ HW1/memo.c
@@ -4,15 +4,15 @@
 #include "xgpiops.h"  //GPIO PS 사용하기 위한 header file (지워도 되는지 실험해보기)
 #include "sleep.h"  //usleep 사용을 위한 header file
 
-int ALU(int step, int opcode);
+int ALU(unsigned int DIP, unsigned int BTN);
 
 int main() {
     static XGpio input, output;
     //static XGpioPs psGpioInstance_ptr;  //PS의 GPIO를 사용하기 위한 pointer
-    //static XGpioPs_Config *GpioConfigPtr;      //PS의 GPIO 설정을 위한 pointer
-    unsigned int Result = 0b0110;      // ALU 결과를 저장할 register
-    unsigned int DIP_SW_data = 0b0000;
-    unsigned int BTN_data = 0b0000;
+    //static XGpioPs_Config *GpioConfigPtr;  //PS의 GPIO 설정을 위한 pointer
+    unsigned int result = 0b0110;  // ALU 결과를 저장할 register
+    unsigned int DIP_SW_data = 0;
+    unsigned int BTN_data = 0;
     int xStatus;
 
     init_platform();  //UART 및 cache 초기화
@@ -20,15 +20,15 @@ int main() {
     //GPIO 0 초기화 : input 부분
     //AXI GPIO_SW Initialization
     //AXI GPIO_Button Initialization
-    xStatus = XGpio_Initialize(&input, XPAR_AXI_GPIO_0_IS_DUAL);
+    xStatus = XGpio_Initialize(&input, XPAR_AXI_GPIO_0_DEVICE_ID);
     if(XST_SUCCESS != xStatus)
-        print("GPIO_SW INIT FAILDED \n\r");
+        printf("GPIO_SW INIT FAILDED \n\r");
 
     //GPIO 1 초기화 : output 부분
     //AXI GPIO_LED Initialization
     xStatus = XGpio_Initialize(&output,XPAR_AXI_GPIO_1_DEVICE_ID);
     if(XST_SUCCESS != xStatus)
-        print("GPIO_LED INIT FAILDED \n\r");
+        printf("GPIO_LED INIT FAILDED \n\r");
 
     //AXI GPIO 입출력 설정
     XGpio_SetDataDirection(&input,1,1);  // 1 : 1st channel, 1 : 입력 -> DIP SW
@@ -37,12 +37,50 @@ int main() {
 
     while(1) {
         DIP_SW_data = XGpio_DiscreteRead(&input, 1);  //switch에서 입력
-        BTN_data = XGpio_DiscreteRead(&input, 2);     //BTN에서 입력
-        print("%b %b\n", DIP_SW_data, BTN_data);
-        
-        XGpio_DiscreteWrite(&output, 1 , Result);       //LED OUT
+        BTN_data = XGpio_DiscreteRead(&input, 2); //BTN에서 입력
+        //printf("%d %d\n", DIP_SW_data, BTN_data);
+        result = ALU(DIP_SW_data, BTN_data);
+        XGpio_DiscreteWrite(&output, 1 , result);   //LED OUT
         usleep(1000);
     }
     cleanup_platform();
     return 0;
+}
+
+int ALU(unsigned int DIP, unsigned int BTN) {
+    switch(DIP){
+        case 0b0000 : 
+            printf("%d %d\n", DIP, BTN);
+        case 0b0100 : 
+            printf("%d %d\n", DIP, BTN);
+        case 0b1000 : 
+            printf("%d %d\n", DIP, BTN);
+        case 0b1100 : 
+            printf("%d %d\n", DIP, BTN);
+        case 0b0001 : 
+            printf("%d %d\n", DIP, BTN);
+        case 0b0101 : 
+            printf("%d %d\n", DIP, BTN);
+        case 0b1001 : 
+            printf("%d %d\n", DIP, BTN);
+        case 0b1101 : 
+            printf("%d %d\n", DIP, BTN);
+        case 0b0010 : 
+            printf("%d %d\n", DIP, BTN);
+        case 0b0110 : 
+            printf("%d %d\n", DIP, BTN);
+        case 0b1010 : 
+            printf("%d %d\n", DIP, BTN);
+        case 0b1110 : 
+            printf("%d %d\n", DIP, BTN);
+        case 0b0011 : 
+            printf("%d %d\n", DIP, BTN);
+        case 0b0111 : 
+            printf("%d %d\n", DIP, BTN);
+        case 0b1011 : 
+            printf("%d %d\n", DIP, BTN);
+        case 0b1111 : 
+            printf("%d %d\n", DIP, BTN);
+    }
+    return 0b0110;
 }
