cb491f463e19ff3c8fbe449a1f6a92eab64ac03f
Jongsang Yoo <jamin0513@gmail.com>
2022-06-01 18:08:59 +0900

    Setting SDK project

---
+++ HW2_2/HW2_2_vivado/HW2_2_vivado.sdk/Hello_world/Debug/src/helloworld.d
@@ -0,0 +1,21 @@
+src/helloworld.o src/helloworld.o: ../src/helloworld.c ../src/platform.h \
+ ../src/platform_config.h \
+ ../../Hello_world_bsp/ps7_cortexa9_0/include/xil_printf.h \
+ ../../Hello_world_bsp/ps7_cortexa9_0/include/xil_types.h \
+ ../../Hello_world_bsp/ps7_cortexa9_0/include/xparameters.h \
+ ../../Hello_world_bsp/ps7_cortexa9_0/include/xparameters_ps.h \
+ ../../Hello_world_bsp/ps7_cortexa9_0/include/bspconfig.h
+
+../src/platform.h:
+
+../src/platform_config.h:
+
+../../Hello_world_bsp/ps7_cortexa9_0/include/xil_printf.h:
+
+../../Hello_world_bsp/ps7_cortexa9_0/include/xil_types.h:
+
+../../Hello_world_bsp/ps7_cortexa9_0/include/xparameters.h:
+
+../../Hello_world_bsp/ps7_cortexa9_0/include/xparameters_ps.h:
+
+../../Hello_world_bsp/ps7_cortexa9_0/include/bspconfig.h:
---
+++ HW2_2/HW2_2_vivado/HW2_2_vivado.sdk/Hello_world/Debug/src/platform.d
@@ -0,0 +1,16 @@
+src/platform.o src/platform.o: ../src/platform.c \
+ ../../Hello_world_bsp/ps7_cortexa9_0/include/xparameters.h \
+ ../../Hello_world_bsp/ps7_cortexa9_0/include/xparameters_ps.h \
+ ../../Hello_world_bsp/ps7_cortexa9_0/include/xil_cache.h \
+ ../../Hello_world_bsp/ps7_cortexa9_0/include/xil_types.h \
+ ../src/platform_config.h
+
+../../Hello_world_bsp/ps7_cortexa9_0/include/xparameters.h:
+
+../../Hello_world_bsp/ps7_cortexa9_0/include/xparameters_ps.h:
+
+../../Hello_world_bsp/ps7_cortexa9_0/include/xil_cache.h:
+
+../../Hello_world_bsp/ps7_cortexa9_0/include/xil_types.h:
+
+../src/platform_config.h:
