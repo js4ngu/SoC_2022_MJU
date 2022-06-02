b7262d336dab982a56215dfb87a5956eae6834b6
Jongsang Yoo <jamin0513@gmail.com>
2022-06-02 21:05:27 +0900

    블록디자인 다시 셋팅

--- HW2_2/HW2_2_vivado/HW2_2_vivado.sdk/Hello_world/Debug/src/helloworld.d
+++ HW2_2/HW2_2_vivado/HW2_2_vivado.sdk/Hello_world/Debug/src/helloworld.d
@@ -1,21 +1,70 @@
-src/helloworld.o src/helloworld.o: ../src/helloworld.c ../src/platform.h \
- ../src/platform_config.h \
- ../../Hello_world_bsp/ps7_cortexa9_0/include/xil_printf.h \
+src/helloworld.o src/helloworld.o: ../src/helloworld.c \
+ ../../Hello_world_bsp/ps7_cortexa9_0/include/xaxidma.h \
+ ../../Hello_world_bsp/ps7_cortexa9_0/include/xaxidma_bdring.h \
+ ../../Hello_world_bsp/ps7_cortexa9_0/include/xstatus.h \
  ../../Hello_world_bsp/ps7_cortexa9_0/include/xil_types.h \
+ ../../Hello_world_bsp/ps7_cortexa9_0/include/xil_assert.h \
+ ../../Hello_world_bsp/ps7_cortexa9_0/include/xaxidma_bd.h \
+ ../../Hello_world_bsp/ps7_cortexa9_0/include/xaxidma_hw.h \
+ ../../Hello_world_bsp/ps7_cortexa9_0/include/xil_io.h \
+ ../../Hello_world_bsp/ps7_cortexa9_0/include/xil_printf.h \
  ../../Hello_world_bsp/ps7_cortexa9_0/include/xparameters.h \
  ../../Hello_world_bsp/ps7_cortexa9_0/include/xparameters_ps.h \
- ../../Hello_world_bsp/ps7_cortexa9_0/include/bspconfig.h
+ ../../Hello_world_bsp/ps7_cortexa9_0/include/bspconfig.h \
+ ../../Hello_world_bsp/ps7_cortexa9_0/include/xpseudo_asm.h \
+ ../../Hello_world_bsp/ps7_cortexa9_0/include/xreg_cortexa9.h \
+ ../../Hello_world_bsp/ps7_cortexa9_0/include/xpseudo_asm_gcc.h \
+ ../../Hello_world_bsp/ps7_cortexa9_0/include/xdebug.h \
+ ../../Hello_world_bsp/ps7_cortexa9_0/include/xil_cache.h \
+ ../../Hello_world_bsp/ps7_cortexa9_0/include/xparameters.h \
+ ../../Hello_world_bsp/ps7_cortexa9_0/include/xil_exception.h \
+ ../../Hello_world_bsp/ps7_cortexa9_0/include/xdebug.h \
+ ../../Hello_world_bsp/ps7_cortexa9_0/include/xscugic.h \
+ ../../Hello_world_bsp/ps7_cortexa9_0/include/xscugic_hw.h \
+ ../../Hello_world_bsp/ps7_cortexa9_0/include/xil_exception.h
 
-../src/platform.h:
+../../Hello_world_bsp/ps7_cortexa9_0/include/xaxidma.h:
 
-../src/platform_config.h:
+../../Hello_world_bsp/ps7_cortexa9_0/include/xaxidma_bdring.h:
 
-../../Hello_world_bsp/ps7_cortexa9_0/include/xil_printf.h:
+../../Hello_world_bsp/ps7_cortexa9_0/include/xstatus.h:
 
 ../../Hello_world_bsp/ps7_cortexa9_0/include/xil_types.h:
+
+../../Hello_world_bsp/ps7_cortexa9_0/include/xil_assert.h:
+
+../../Hello_world_bsp/ps7_cortexa9_0/include/xaxidma_bd.h:
+
+../../Hello_world_bsp/ps7_cortexa9_0/include/xaxidma_hw.h:
+
+../../Hello_world_bsp/ps7_cortexa9_0/include/xil_io.h:
+
+../../Hello_world_bsp/ps7_cortexa9_0/include/xil_printf.h:
 
 ../../Hello_world_bsp/ps7_cortexa9_0/include/xparameters.h:
 
 ../../Hello_world_bsp/ps7_cortexa9_0/include/xparameters_ps.h:
 
 ../../Hello_world_bsp/ps7_cortexa9_0/include/bspconfig.h:
+
+../../Hello_world_bsp/ps7_cortexa9_0/include/xpseudo_asm.h:
+
+../../Hello_world_bsp/ps7_cortexa9_0/include/xreg_cortexa9.h:
+
+../../Hello_world_bsp/ps7_cortexa9_0/include/xpseudo_asm_gcc.h:
+
+../../Hello_world_bsp/ps7_cortexa9_0/include/xdebug.h:
+
+../../Hello_world_bsp/ps7_cortexa9_0/include/xil_cache.h:
+
+../../Hello_world_bsp/ps7_cortexa9_0/include/xparameters.h:
+
+../../Hello_world_bsp/ps7_cortexa9_0/include/xil_exception.h:
+
+../../Hello_world_bsp/ps7_cortexa9_0/include/xdebug.h:
+
+../../Hello_world_bsp/ps7_cortexa9_0/include/xscugic.h:
+
+../../Hello_world_bsp/ps7_cortexa9_0/include/xscugic_hw.h:
+
+../../Hello_world_bsp/ps7_cortexa9_0/include/xil_exception.h:
